49a8c6d42ed5b7609347a2c19139bf778e868dfd
Tiago <tiagooom88@gmail.com>
2024-04-19 14:47:36 -0200

    usando loop na home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,4 +2,7 @@
 @section('title', 'Essa é a noss pagina HOME')
 @section('conteudo')
     <h1> Essa é nossa home </h1>
+    @for ($i = 0; $i < 15; $i++)
+        valor atual é {{ $i }} <br>
+    @endfor
 @endsection
