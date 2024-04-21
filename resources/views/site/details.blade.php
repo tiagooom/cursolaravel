a602d08361db125184fc505d6f9aa68db3effba9
Tiago <tiagooom88@gmail.com>
2024-04-21 15:36:06 -0200

    adicionando usuario e categorio do produto

--- resources/views/site/details.blade.php
+++ resources/views/site/details.blade.php
@@ -6,10 +6,13 @@
     <div class="col s12 m6">
         <img src="{{ $produto->imagem }}" class="responsive-img">
     </div>
-    
+
     <div class="col s12 m6">
         <h1>{{ $produto->nome }}</h1>
         <p>{{ $produto->descricao }}</p>
+        <p> Postado por: {{ $produto->user->firstName }} <br>
+            Categoria: {{ $produto->categoria->nome }}
+        </p>
         <button class="btn orange btn-large"> Comprar </button>
     </div>
 </div>
